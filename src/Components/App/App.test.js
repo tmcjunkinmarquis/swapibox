bdede8379e02b42d8a91af4f12900297cbc55e81
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-13 11:35:14 -0600

    Setup and confirm some tests work

---
+++ src/Components/App/App.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App.js';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+    let app;
+    
+
+    beforeEach( async () => {
+        app = shallow(<App />, {disableLifecycleMethods: true})
+
+    })
+
+    it.skip('should match the snapshot', () => {
+        const appSnapShot = renderer.create(app).JSON;
+
+        expect(appSnapShot).toMatchSnapshot();
+    })
+    it('should have default states', async () => {
+        // console.log(wrapper)
+       const wrapper = await shallow(<App />)
+        expect(wrapper.state().film).toEqual([])
+        expect(wrapper.state().people).toEqual([])
+        expect(wrapper.state().vehicles).toEqual([])
+        expect(wrapper.state().planets).toEqual([])
+        expect(wrapper.state().cleanedCardData).toEqual([])
+
+    })
+
+    it('should setState', () => {
+
+    })
+
+    it('should call fetchApi', () => {
+
+    })
+
+    it('should call checkCardType with the correct params', () => {
+
+    })
+    
+    it('should update vehicle ste when getVehicleData is called', () => {
+
+    })
+
+    it('conbineObj should return correct Obj')
+
+})
